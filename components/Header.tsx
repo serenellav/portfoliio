a189c4c43472e4f33df8027385bec33dffbde82e
Dario Vettura <73988414+dariovettura@users.noreply.github.com>
2022-04-19 19:12:53 +0200

    ADD:titolo

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,9 @@ const Header = (): JSX.Element => (
        
         <Link href="/" >
             <a> 
-            <h1 style={{fontWeight:"800",margin:0}}>PORT<span style={{color:"#ff8800"}}>FOLIO</span></h1>
+            <h1 style={{fontWeight:"800",margin:0}}>PAOLO
+            <span style={{color:"#ff8800"}}>MINOPOLI</span>
+            </h1>
             </a>
         </Link>
         <SwipableMenu/>
