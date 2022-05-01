54906f6b1ef923acb7888adfdb067d5f05204fdc
Paolo589 <kappa589@hotmail.it>
2022-05-01 10:29:16 +0200

    FIX: galleria

--- components/ContentsLayout.tsx
+++ components/ContentsLayout.tsx
@@ -6,59 +6,52 @@ import ReactPlayer from "react-player";
 
 interface Props {
 
-    content?: any,
+	content?: any,
 
 }
 
 const ContentLayout: React.FC<Props> = ({ content }) => {
 
-    const contentToArray = () => {
-        if (content && content?.acf?.contents)
-            return Object.keys(content?.acf?.contents)
-                .map(function (key) {
-                    return content.acf.contents[key];
-                })
-        else { return [] }
-    }
+	const contentToArray = () => {
+		if (content && content?.acf?.contents)
+			return Object.keys(content?.acf?.contents)
+				.map(function (key) {
+					return content.acf.contents[key];
+				})
+		else { return [] }
+	}
 
-    const lay = contentToArray()
+	const lay = contentToArray()
 
-    console.log({ lay }); // ["hello", "this is", "javascript!"]
+	console.log({ lay }); // ["hello", "this is", "javascript!"]
 
-    const contentRender = (obj: any, i: number) => {
-        if (obj && obj.type && obj.type == "image")
-            return <img key={i} src={obj.url} alt="" style={{ width: "100%", height: "auto" }} />
-        if (obj && obj.type && obj.type == "video")
-            return <div><ReactPlayer
-            muted
-            className="video_player"
-            src={obj.url}
-            playsinline
-            loop
-        /></div> 
-    }
+	const contentRender = (obj: any, i: number) => {
+		if (obj && obj.type && obj.type == "image")
+			return <div key={i} ><img src={obj.url} alt="" style={{ width: "100%", height: "auto" }} />
+			{obj.description &&	<p className='post-content-description'>{obj.description}</p>}</div>
+		if (obj && obj.type && obj.type == "video")
+			return <div key={i}><ReactPlayer
+				muted
+				className="video_player"
+				src={obj.url}
+				playsinline
+				loop
+			/>
+					{obj.description &&	<p className='post-content-description'>{obj.description}</p>}</div>
+	else
+			return <div key={i} className='post-content-container' dangerouslySetInnerHTML={{ __html: obj }} />
+	}
 
 
 
 
-        return (
-            <>
-                {lay.map((obj, i) =>
-                    contentRender(obj, i)
-                )}
-                  <video controls autoPlay loop muted>
-          <source src="https://paolominopoli.altervista.org/wp-content/uploads/2022/04/Video.mp4" type="video/mp4"></source>
-        </video>
-              
-                <ReactPlayer
-            muted
-            className="video_player"
-            src="https://paolominopoli.altervista.org/wp-content/uploads/2022/04/Video.mp4"
-            playsinline
-            loop
-        />
-            </>
-        );
-    }
+	return (
+		<>
+			{lay.map((obj, i) =>
+				contentRender(obj, i)
+			)}
+		</>
+	);
+}
 
-    export default ContentLayout;
+export default ContentLayout;
