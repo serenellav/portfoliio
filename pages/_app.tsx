d036cf3d937fa5728c04ad0897139f4f6fbcd341
Paolo589 <kappa589@hotmail.it>
2022-05-01 16:54:29 +0200

    ADD: home page seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,9 +5,32 @@ import { Provider } from "react-redux"
 import { store } from '../store/index'
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return <Provider store={store}>
+       <NextSeo
+       title = 'Portfolio'
+       titleTemplate = 'Paolo Minopoli | %s'
+        defaultTitle="Paolo Minopoli"
+        description="Creatore di modelli 3D"
+        canonical="https://paolominopoli.vercel.app/"
+        openGraph={{
+          url: "https://paolominopoli.vercel.app/",
+          title: "Paolo Minopoli",
+          description: "Creatore di modelli 3D",
+          images: [
+            {
+              url: "/avatar-paolo.png",
+              width: 800,
+              height: 420,
+              alt: "Paolo Minopoli",
+            },
+          ],
+        }}
+       
+      />
+
     <Header></Header>
     <Sidebar></Sidebar>
     <AnimatePresence exitBeforeEnter >
