24b7fd53f91d9b4f748cd4801637c638bcd0b4b7
Paolo589 <kappa589@hotmail.it>
2022-05-04 20:20:49 +0200

    test-font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { AnimatePresence, AnimateSharedLayout } from "framer-motion";
 import type { AppProps } from 'next/app'
 import { Provider } from "react-redux"
 import { store } from '../store/index'
+import Head from 'next/head';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import { NextSeo } from 'next-seo';
@@ -12,47 +13,53 @@ import Link from 'next/link';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return <Provider store={store}>
-       <NextSeo
-       title = 'Portfolio'
-       titleTemplate = 'Paolo Minopoli | %s'
-        defaultTitle="Paolo Minopoli"
-        description="3D Designer"
-        canonical="https://paolominopoli.vercel.app/"
-        openGraph={{
-          type: 'website',
-          url: "https://paolominopoli.vercel.app/",
-          title: "Paolo Minopoli",
-          description: "3D Designer",
-          images: [
-            { width:400,
-              height:400,
-              url: "https://paolominopoli.vercel.app/avatar-paolo.png",
-              alt: "Paolo Minopoli",
-            },
-          ],
-        }}
-       
-      />
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet" />
+    </Head>
+    <NextSeo
+      title='Portfolio'
+      titleTemplate='Paolo Minopoli | %s'
+      defaultTitle="Paolo Minopoli"
+      description="3D Designer"
+      canonical="https://paolominopoli.vercel.app/"
+      openGraph={{
+        type: 'website',
+        url: "https://paolominopoli.vercel.app/",
+        title: "Paolo Minopoli",
+        description: "3D Designer",
+        images: [
+          {
+            width: 400,
+            height: 400,
+            url: "https://paolominopoli.vercel.app/avatar-paolo.png",
+            alt: "Paolo Minopoli",
+          },
+        ],
+      }}
+
+    />
 
     <Header></Header>
     <Sidebar></Sidebar>
     <CookieConsent
-    contentStyle={{margin:"0px",position:"relative"}}
-  location="bottom"
-  buttonText="Ok,Ho capito."
-  cookieName="myAwesomeCookieName2"
-  style={{ background: "#000" ,fontSize: "13px",fontWeight: 800}}
-  buttonStyle={{ color: "#fff",fontWeight: 800, fontSize: "13px",background: "#008069",margin:"5px" }}
-  expires={150}
->
-  Questo sito usa cookies per migliorare la tua esperienza di navigazione.
- <Link href="/privacy-policy"><span style={{ fontSize: "13px",color:"#008069",textDecoration:"underline"}} >Vedi cookies e privacy policy</span></Link>
-</CookieConsent>
+      contentStyle={{ margin: "0px", position: "relative" }}
+      location="bottom"
+      buttonText="Ok,Ho capito."
+      cookieName="myAwesomeCookieName2"
+      style={{ background: "#000", fontSize: "13px", fontWeight: 800 }}
+      buttonStyle={{ color: "#fff", fontWeight: 800, fontSize: "13px", background: "#008069", margin: "5px" }}
+      expires={150}
+    >
+      Questo sito usa cookies per migliorare la tua esperienza di navigazione.
+      <Link href="/privacy-policy"><span style={{ fontSize: "13px", color: "#008069", textDecoration: "underline" }} >Vedi cookies e privacy policy</span></Link>
+    </CookieConsent>
 
-   
+
     <AnimatePresence exitBeforeEnter >
       <Component key={router.route} {...pageProps} />
-    
+
     </AnimatePresence>
 
   </Provider>
