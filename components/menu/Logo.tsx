45b020d7ea8ca73e0a7ef3d80618cfce9e0042ee
Paolo589 <kappa589@hotmail.it>
2022-05-05 17:15:25 +0200

    fix logo url

--- components/menu/Logo.tsx
+++ components/menu/Logo.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 const Logo = (): JSX.Element => (
 	<div>
-		<Link href="/" >
+		<Link href="/" scroll={false}>
 			<a>
 				<span className="logo logo-1">PAOLO
 					<span className="logo-2">PIEZ</span>
