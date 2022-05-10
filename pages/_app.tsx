127a908c3e542b7ce3c9392ace1fa5ffe9d8d2e7
Dario Vettura <73988414+dariovettura@users.noreply.github.com>
2022-05-10 17:08:44 +0200

    ADD: barlow font added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ function MyApp({ Component, pageProps, router }: AppProps) {
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;500&family=Montserrat:ital,wght@0,100;0,200;0,300;0,500;1,100;1,200;1,500&display=swap" rel="stylesheet" />
+      <link href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,500;0,600;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
     </Head>
     <NextSeo
       title='Portfolio'
